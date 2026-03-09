28832b2df8d6ce9584637f8ac48eefb18dc38998
Elena Kubaieva <kubaevaelena184@gmail.com>
2026-03-09 21:20:51 +0200

    Session 1

--- src/org/prog/session1/Main.java
+++ src/org/prog/session1/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
 //        System.out.println("Hello World!");
 
-        int i = 10;
+//        int i = 10;
 
 //        System.out.println(5 - 1);
 //        System.out.println(5 + 1);
@@ -23,70 +23,82 @@ public class Main {
 //        int k = 10*31;
 //        System.out.println(i);
 //        System.out.println(i - 200);
-
-        boolean b1 = true;
-        boolean b2 = false;
-
-        int value1 = 3;
-        int value2 = 4;
-        int value3 = 1;
-
-        // == - values are equal
-        // != - values are NOT equal
-        // > - more than
-        // < - less than
-        // <= - less or equals than
-        // >= - more or equals than
-        // <> - not equals
-
-//        if (value1 == value2) {
-//            System.out.println("Values 1 & 2 are equal!");
-//        } else if (value1 == value3) {
-//            System.out.println("Values 1 & 3 are equal!");
+//
+//        boolean b1 = true;
+//        boolean b2 = false;
+//
+//        int value1 = 3;
+//        int value2 = 4;
+//        int value3 = 1;
+//
+//        // == - values are equal
+//        // != - values are NOT equal
+//        // > - more than
+//        // < - less than
+//        // <= - less or equals than
+//        // >= - more or equals than
+//        // <> - not equals
+//
+////        if (value1 == value2) {
+////            System.out.println("Values 1 & 2 are equal!");
+////        } else if (value1 == value3) {
+////            System.out.println("Values 1 & 3 are equal!");
+////        } else {
+////            System.out.println("Something went wrong!");
+////        }
+//
+////        if (value1 < value2) {
+////            System.out.println("Value 1 is less than Value 2!");
+////        } else if (value1 < value3) {
+////            System.out.println("Value 1 is less than Value 3!");
+////        } else {
+////            System.out.println("something else");
+////        }
+////        boolean b4 = value2 == value3;
+////        System.out.println(b4);
+////        if (b4) {
+////            System.out.println("b4 is true");
+////        } else {
+////            System.out.println("b4 is false");
+////        }
+////
+////
+//
+////        boolean b5 = false;
+////        boolean b6 = false;
+////        boolean b7 = true;
+//
+//        // && - AND
+//        // || - OR
+//
+////        System.out.println(b5 && b6 && b7);
+////        System.out.println(b5 || b6 || b7);
+////        System.out.println(b5 && b6 || b7);
+////        System.out.println(b5 || b6 && b7);
+//
+//        int v1 = 10;
+//        int v2 = 3;
+//        int v3 = 4;
+//        int v4 = 5;
+//
+//        if ((v1 % v2 == 1) && (v1 % v3 == 2)) {
+//            System.out.println("OK");
 //        } else {
-//            System.out.println("Something went wrong!");
-//        }
-
-//        if (value1 < value2) {
-//            System.out.println("Value 1 is less than Value 2!");
-//        } else if (value1 < value3) {
-//            System.out.println("Value 1 is less than Value 3!");
-//        } else {
-//            System.out.println("something else");
-//        }
-//        boolean b4 = value2 == value3;
-//        System.out.println(b4);
-//        if (b4) {
-//            System.out.println("b4 is true");
-//        } else {
-//            System.out.println("b4 is false");
+//            System.out.println("ERROR");
 //        }
 //
-//
+//        System.out.println("CODE WORKS!!!");
 
-//        boolean b5 = false;
-//        boolean b6 = false;
-//        boolean b7 = true;
-
-        // && - AND
-        // || - OR
-
-//        System.out.println(b5 && b6 && b7);
-//        System.out.println(b5 || b6 || b7);
-//        System.out.println(b5 && b6 || b7);
-//        System.out.println(b5 || b6 && b7);
-
-        int v1 = 10;
-        int v2 = 3;
-        int v3 = 4;
-        int v4 = 5;
-
-        if ((v1 % v2 == 1) && (v1 % v3 == 2)) {
-            System.out.println("OK");
-        } else {
-            System.out.println("ERROR");
+  System.out.println("Hello homework");
+int i = 10;
+int j = 20;
+if (i == j) {
+    System.out.println("i == j are OK");
+}else if ( i!= j){
+    System.out.println("i != j are OK");
         }
-
-        System.out.println("CODE WORKS!!!");
+else {
+    System.out.println("NOT OK");
+}
     }
 }
